a7b57916a9a1f39970190878aa7ca494ffdb1c59
Brandon Walsh <bmwalshy@gmail.com>
2025-11-28 10:08:31 -0500

    Create dockerfile and docker-compose

--- Makefile
+++ Makefile
@@ -13,17 +13,25 @@ CLIENT_OUT_DIR := client/src/proto/generated
 TS_PROTO_PLUGIN := client/node_modules/.bin/protoc-gen-ts_proto
 TS_OPTS := esModuleInterop=true,forceLong=string,outputServices=false
 
-.PHONY: all proto clean help
+.PHONY: all proto_go proto_dev clean help
 
-all: proto
+all: proto_dev
 
 clean:
 	@echo "Cleaning generated"
 	@find $(GO_OUT_DIR) -name "*.pb.go" -delete
 	@find $(CLIENT_OUT_DIR) -name "*.ts" -delete
 
-proto: $(PROTO_FILES)
+proto_go: $(PROTO_FILES)
 	@echo "Compiling Go Protobuf files"
+	protoc \
+		--proto_path=$(PROTO_DIR) \
+		--go_out=$(GO_OUT_DIR) \
+		--go_opt=$(GO_OPTS) \
+		$(PROTO_FILES)
+
+proto_dev: $(PROTO_FILES)
+	@echo "Compiling Go And TS Protobuf files"
 	protoc \
 		--proto_path=$(PROTO_DIR) \
 		--go_out=$(GO_OUT_DIR) \
@@ -33,7 +41,6 @@ proto: $(PROTO_FILES)
     --ts_proto_opt=$(TS_OPTS) \
 		--ts_proto_out=$(CLIENT_OUT_DIR) \
 		$(PROTO_FILES)
-	@echo "Compiling TS Protobuf files"
 
 
 help:
--- db.go
+++ db.go
@@ -1,38 +1,50 @@
 package main 
 
 import (
-  "time"
-  "context"
-  "github.com/redis/go-redis/v9"
-  "go.mongodb.org/mongo-driver/v2/mongo"
-  "go.mongodb.org/mongo-driver/v2/mongo/options"
-
+	"os"
+	"time"
+	"context"
+	"github.com/redis/go-redis/v9"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 func makeRedisClient() *redis.Client {
-  rdb := redis.NewClient(&redis.Options{
-    Addr:     "localhost:6379",
-    Password: "", // no password set
-    DB:       0,  // use default DB
-  })
-  return rdb
+	const defaultRedisAddr = "localhost:6379" 
+	redisAddr := os.Getenv("REDIS_ADDR")
+	if redisAddr == "" {
+		redisAddr = defaultRedisAddr
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	return rdb
 }
 
 func makeMongoClient(ctx context.Context) *mongo.Client {
-  context, cancel := context.WithTimeout(ctx, 10*time.Second)
-    defer cancel()
+	context, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
 
-  client, err := mongo.Connect(options.Client().ApplyURI("mongodb://localhost:27017"))
-    if err != nil {
-        //return nil, fmt.Errorf("mongo connect error: %w", err)
-        println("mongo connect error")
-    }
+	const defaultMongoURI = "mongodb://localhost:27017/whiteboard" 
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		mongoURI = defaultMongoURI
+	}
 
-    // Ping to verify we can actually reach the server
-    if err := client.Ping(context, nil); err != nil {
-        //return nil, fmt.Errorf("mongo ping error: %w", err)
-        println("mongo ping error")
-    }
+	client, err := mongo.Connect(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		//return nil, fmt.Errorf("mongo connect error: %w", err)
+		println("mongo connect error")
+	}
 
-    return client
+	// Ping to verify we can actually reach the server
+	if err := client.Ping(context, nil); err != nil {
+		//return nil, fmt.Errorf("mongo ping error: %w", err)
+		println("mongo ping error")
+	}
+
+	return client
 }
