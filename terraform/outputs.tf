6195ab385b1709539703b6d9fc56a65e1782109f
Brandon Walsh <bmwalshy@gmail.com>
2025-11-28 22:10:39 -0500

    Output alb dns name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "ci_access_key_id" {
   description = "The Access Key ID for the CI/CD user (MUST be saved to GitHub Secrets)."
   value       = aws_iam_access_key.ci_key.id
 }
+
+output "alb_dns_name" {
+  description = "The DNS name of the Application Load Balancer"
+  value       = aws_lb.app_lb.dns_name
+}
